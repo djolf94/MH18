53f4643dd41d23d495bc7d84832330f0c62471c4
Djole94 <djoletheomniscient@gmail.com>
2018-04-21 10:17:17 +0200

    model, v-if, v-for

---
+++ src/components/test.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="test">
+    <h1>{{title}}</h1>
+    <p v-if="showName">Best name ever: {{user.firstName}}</p>
+    <p v-else="showName">Best singer: {{user.lastName}}</p>
+    <ul>
+      <li v-for="item in items">{{item.title}}</li>
+    </ul>
+    <button v-on:click="greet('Pozdravi je Pozdravi')">Say greeting</button><br />
+    <label>First name:</label><input type="text" v-model="user.firstName"></input>
+    <br />
+    <label>Last name:</label><input type="text" v-model="user.lastName"></input>
+    <h3>{{fullName}}</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test',
+    data() {
+      return {
+        title: "Ovo je naslov",
+        user: {
+          firstName: "Djordje",
+          lastName: "Balasevic"
+        },
+        showName: true,
+        items: [
+          {title: "Item one"},
+          {title: "Item two"},
+          {title: "Item three"}
+        ]
+      }
+    },
+    methods: {
+      greet: function(atr) {
+        alert(atr);
+      },
+      spacePressed: function(e) {
+        console.log("Pritisnuli smo space");
+      }
+    },
+    computed: {
+      fullName: function() {
+        return this.user.firstName + " " + this.user.lastName;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+
+</style>
