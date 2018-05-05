fcc48c5b9db08017655b4c76b2fea5a49d164508
Nikola Dimic <dimic.nikola@gmail.com>
2018-05-05 23:16:40 +0200

    login page changes

--- vue/src/components/login.vue
+++ vue/src/components/login.vue
@@ -3,23 +3,21 @@
         <h1> {{title}} </h1>
 
         <!-- ide .prevent da ne bi slalo po difoltu iz html-a -->
-		<form v-on:submit.prevent = "logUser"> 
-            <label for="inputUsername">Username</label>
-    		<input type="text" id="inputUsername" placeholder="Username"
-    		name="username" v-model="newUser.userName">
-            <br/>  
-
-            <label for="inputPassword4">Password</label>
-      		<input type="password" id="inputPassword4" placeholder="Password"
-      		name="password" v-model="newUser.password">
-            <br/>    			
-
-             <!-- poruka o gresci -->
-            <p v-if="ind"> {{message}} </p>
-   			  				
-    	    <button type="submit" value="Log User">Log in</button>
-		</form>
-        
+        <div id="text_form">    
+            <form v-on:submit.prevent = "logUser">
+                <div class="form-group">
+                    <label for="inputUsername">Username</label>
+                    <input class="form-control" type="text" id="inputUsername" placeholder="Username" name="username" v-model="newUser.userName">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your data with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword4">Password</label>
+                    <input class="form-control" type="password" id="inputPassword4" placeholder="Password" name="password" v-model="newUser.password">
+                </div>
+                <p v-if="ind"> {{message}} </p>
+                <button type="submit" class="btn btn-dark" value="Log User">Login</button>
+            </form>
+        </div>
 
     </div>
 </template>
@@ -49,7 +47,7 @@
             checkPassword: function(response) {
                 if (response.data == null) {
                     this.ind = true;
-                    this.message = "Ne postoji user sa ovim username-om";
+                    this.message = "Non existent user";
                 }
                 else 
                     if (response.data.password == this.newUser.password) {
@@ -59,9 +57,37 @@
                     }
                     else {
                         this.ind = true;
-                        this.message = "Pogresan password";
+                        this.message = "Wrong password";
                     }
             }
         }
     }
 </script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1, h2 {
+        margin: 3%;
+        font-weight: normal;
+        text-align: center
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+    #text_form{
+        width:50%;
+        margin: auto
+        
+    }
+    #text_form input {
+        width: 100%;
+        clear: both;
+    }
+    #invalidSignIn {
+        color: red;
+    }
+</style>
