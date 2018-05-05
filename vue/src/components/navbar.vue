6956e9cd1488c05be7aca49b71ca5650e27f8ffd
Nikola Dimic <nikoladimic@Nikolas-MacBook-Pro.local>
2018-05-05 22:25:41 +0200

    navbar changes, bootstrap added

--- vue/src/components/navbar.vue
+++ vue/src/components/navbar.vue
@@ -1,29 +1,44 @@
 <template>
-    <div class = "navbar">
-        <router-link to="/">Main</router-link>
-        <router-link to="/login">Log in</router-link>
-        <router-link to="/register">Register</router-link>
-        <button v-on:click="logOut()">Log out</button>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
+
+        <router-link class="navbar-brand" to="/">
+            <img src="../assets/logo.png" width="30" height="30" alt="">
+        </router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/" active-class="active" exact> Home</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/login" active-class="active" exact>Login</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/register" active-class="active" exact>Register</router-link>
+                </li>
+            </ul>
+        </div>
+        <button class="btn btn-outline-success my-2 my-sm-0" v-on:click="logOut()">Logout</button>
+    </nav>
 </template>
 
 <script>
-    export default {
-        name: 'navbar',
-        data () {
-            return {
-
-            }
-        },
-        methods: {
-            logOut: function() {
-                if (this.$auth.isAuthenticated()) {
-                    this.$auth.destroyUserLS();
-                    this.$router.push('/login');
-                }
-                else
-                    alert('Nisi ulogovan');
-            }
-        }
+export default {
+  name: "navbar",
+  data() {
+    return {};
+  },
+  methods: {
+    logOut: function() {
+      if (this.$auth.isAuthenticated()) {
+        this.$auth.destroyUserLS();
+        this.$router.push("/login");
+      } 
+      else 
+        alert("You are not logged in");
     }
+  }
+};
 </script>
