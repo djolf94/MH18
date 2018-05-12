6069d13b041927372056d96c6019c7bf748c0af9
ivke080 <ivke11080@gmail.com>
2018-05-12 16:15:18 +0200

    dodat bus

---
+++ vue/src/components/users.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="users">
+        <p>Kurac</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'users',
+        data() {
+            return {}
+        }
+    }
+</script>
